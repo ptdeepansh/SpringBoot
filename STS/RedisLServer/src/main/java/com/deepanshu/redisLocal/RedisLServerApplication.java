8a7f1a818f643583d47f550b993daf29c81f5173
Deepanshu Sharma <deepanshu.sharma9891@gmail.com>
2025-07-25 18:21:24 +0530

    Add files via upload

---
+++ STS/RedisLServer/src/main/java/com/deepanshu/redisLocal/RedisLServerApplication.java
@@ -0,0 +1,12 @@
+package com.deepanshu.redisLocal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisLServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedisLServerApplication.class, args);
+	}
+}
