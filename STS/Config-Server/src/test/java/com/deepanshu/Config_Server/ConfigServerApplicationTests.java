ce533df08d37ba40d73df458af1acef169cc39ab
Deepanshu Sharma <deepanshu.sharma9891@gmail.com>
2025-07-30 19:18:13 +0530

    Add files via upload

---
+++ STS/Config-Server/src/test/java/com/deepanshu/Config_Server/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deepanshu.Config_Server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
