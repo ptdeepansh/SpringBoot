9ba0f65c9ea3e983adc0e4ea545841fe11cec1e8
Deepanshu Sharma <deepanshu.sharma9891@gmail.com>
2025-07-30 19:20:44 +0530

    Add files via upload

---
+++ STS/SimpleServer/src/main/java/com/deepanshu/simpleserver/database/mongoDB/MuserData.java
@@ -0,0 +1,108 @@
+package com.deepanshu.simpleserver.database.mongoDB;
+
+
+import java.util.Date;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "User")
+public class MuserData {
+	@Id
+	private String id;
+	
+	@Indexed
+	private String name;
+	
+	private String state, email_id;
+	private Integer total_match, total_kills, age;
+	private Long phone_no;
+	private Date joined_date;
+	
+	public MuserData(){}
+	
+	public MuserData(String name, String state, String email_id, Integer totalmatches, Integer totalkills, Long phone_no, Integer age, Date date) {
+		this.name = name;
+		this.state = state;
+		this.email_id = email_id;
+		this.total_match = totalmatches;
+		this.total_kills = totalkills;
+		this.phone_no = phone_no;
+		this.age = age;
+		this.joined_date = date;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public String getEmail_id() {
+		return email_id;
+	}
+
+	public void setEmail_id(String email_id) {
+		this.email_id = email_id;
+	}
+
+	public Integer getTotalmatches() {
+		return total_match;
+	}
+
+	public void setTotalmatches(Integer totalmatches) {
+		this.total_match = totalmatches;
+	}
+
+	public Integer getTotalkills() {
+		return total_kills;
+	}
+
+	public void setTotalkills(Integer totalkills) {
+		this.total_kills = totalkills;
+	}
+
+	public Long getPhone_no() {
+		return phone_no;
+	}
+
+	public void setPhone_no(long phone) {
+		this.phone_no = phone;
+	}
+
+	public Integer getAge() {
+		return age;
+	}
+
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+
+	public Date getJoined_date() {
+		return joined_date;
+	}
+
+	public void setJoined_date(Date joined_date) {
+		this.joined_date = joined_date;
+	}
+
+}
---
+++ STS/SimpleServer/src/main/java/com/deepanshu/simpleserver/services/SecurityConfig.java
@@ -0,0 +1,178 @@
+package com.deepanshu.simpleserver.services;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.client.RestTemplate;
+
+import lombok.AllArgsConstructor;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import com.deepanshu.redisLocal.redisDB.*;
+
+@Configuration
+@AllArgsConstructor
+@EnableWebSecurity
+public class SecurityConfig{
+	
+//	@Autowired
+//	private UserRepository usrRepo;
+//	
+	
+//	private RestTemplate restTemplate;
+	
+	@Bean
+	public UserDetailsService userDetailsService(UserRepository userRepo) {
+		List<UserDetails> users = new ArrayList<>();
+//		RestTemplate restTemplate = new RestTemplate();
+//		restTemplate.getForEntity("localhost:8080/auth", ArrayList.class);
+//		System.out.print(restTemplate);
+		
+		for (UserAuthData user : userRepo.findAll()) {
+//			if (user.getUsername() == null || user.getPassword() == null) {
+////	             Skip invalid users or throw an error with meaningful message
+//				System.out.println("The DB is empty!");
+//	            continue;
+//	        }
+//			System.out.println("Now data is here!");
+			UserDetails details = User
+				.withUsername(user.getUsername())
+				.password("{noop}" + user.getPassword()) // use encoded version in prod
+				.roles(user.getRole())
+				.build();
+			users.add(details);
+		}
+
+		return new InMemoryUserDetailsManager(users);
+	}
+	
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+		return http
+				.csrf(AbstractHttpConfigurer::disable)
+//				.authenticationProvider(authenticationProvider())
+			.formLogin(httpForm -> httpForm
+					.loginPage("/signIn")
+					.loginProcessingUrl("/signIn")
+					.defaultSuccessUrl("/signIn?success=true", true)
+					.failureUrl("/signIn?error=true")
+					.permitAll()
+			)
+			.logout(logout -> logout
+					.logoutUrl("/logout")
+					.logoutSuccessUrl("/signIn?logout=true")
+					.permitAll()
+				)
+			.authorizeHttpRequests(registry -> {
+				registry.requestMatchers("/signUp", "SignUp.html", "SignIn.html", "/BgImage.jpg", "/favicon.jpg", "/css/**", "/images/**", "/", "/user/get").permitAll();
+				registry.requestMatchers("/addData.html").hasRole("ADMIN");
+				registry.requestMatchers("/user/**").hasAnyRole("USER", "ADMIN");
+				registry.anyRequest().authenticated();
+			})
+			.build();
+	}
+	
+//	@Bean
+//	public PasswordEncoder passwordEncoder() {
+//      return new BCryptPasswordEncoder();
+//	}
+}
+	
+//	private final UserAuthService appUserService;
+//
+//    public SecurityConfig(UserAuthService appUserService) {
+//        this.appUserService = appUserService;
+//    }
+//	
+//	@Bean
+//	public UserDetailsService userDetailsService() {
+//		return appUserService;
+//	}
+//	
+//	@Bean
+//	public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+//	    return config.getAuthenticationManager();
+//	}
+//	
+//	@Bean
+//	public AuthenticationProvider authenticationProvider() {
+//		DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+//		provider.setUserDetailsService(appUserService);
+//		provider.setPasswordEncoder(passwordEncoder());
+//		return provider;
+//	}
+//	
+//	@Bean
+//	public PasswordEncoder passwordEncoder() {
+//		return NoOpPasswordEncoder.getInstance();
+//	}
+	
+//	private UserAuthService userAuthService;
+////	@Override
+//	protected void configure(HttpSecurity http) throw Exception{
+//		http .authorizeRequest()
+//				.antMatchers("/login").permitAll()
+//				.anyRequest().authenticated()
+//				.and() 
+//					.fromLogin().LoginPage("/login")
+//				.and() 
+//					.logout().permitAll();
+//	}
+//	
+////	@Override
+//	protected void congigure(AuthenticationManagerBuilder auth) {
+//		auth.userDetailsService(userAuthService).passwordEncoder(new BCryptPasswordEncoder());
+//	}
+	
+
+//	@Bean
+//	public UserDetailsService userDetailsService(String username) {
+//		Optional<UserAuthData> user = usrRepo.findById(username);
+//		if(user.isPresent()) {
+//			var userObj = user.get();
+//			return new InMemoryUserDetailsManager( User.builder()
+//				   .username(username)
+//				   .password(userObj.getPassword())
+//				   .roles(userObj.getRole())
+//				   .build() );	   
+//		}
+//		return new InMemoryUserDetailsManager();
+//	}
+
+//	"/*", "/*.js"
+
+//
+//    // Used by Spring internally for authentication
+//    @Bean
+//    public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig) throws Exception {
+//        return authConfig.getAuthenticationManager();
+//    }
+
+
+
+
+//.csrf().disable()
+//.authorizeHttpRequests(auth -> auth
+//    .requestMatchers("/signin", "/signup", "/style.css", "/user/g-login", "/storeData.html",
+//            "/fonts/**", "/BgImage.jpg","login.html","home.html").permitAll()
+//    .requestMatchers("/", "/user/**").hasAuthority("Admin")
+//    .anyRequest().authenticated()
+//)
+//.formLogin(form -> form
+//    .loginPage("/signin")
+//    .defaultSuccessUrl("/user/get", true)
+//    .permitAll()
+//)
+//.logout(logout -> logout
+//    .logoutUrl("/logout")
+//    .logoutSuccessUrl("/signin")
+//    .permitAll()
+//);
