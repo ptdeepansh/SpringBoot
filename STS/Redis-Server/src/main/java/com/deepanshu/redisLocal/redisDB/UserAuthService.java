daa149e773f9b89b3b82abd758b2515015bb167e
Deepanshu Sharma <deepanshu.sharma9891@gmail.com>
2025-08-01 18:01:25 +0530

    Add files via upload

---
+++ STS/Redis-Server/src/main/java/com/deepanshu/redisLocal/redisDB/UserAuthService.java
@@ -0,0 +1,43 @@
+package com.deepanshu.redisLocal.redisDB;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserAuthService {
+	
+	@Autowired
+	private final UserRepository userRepository;
+	
+	public UserAuthService(UserRepository userRepository) {
+		this.userRepository = userRepository;
+	}
+	
+	public UserAuthData createUser(UserAuthData user) {
+		return userRepository.save(user);
+	}
+	
+	public Optional<UserAuthData> getUser(String id){
+		return userRepository.findById(id);
+	}
+	
+	public Iterable<UserAuthData> getAllUsers(){
+		return userRepository.findAll();
+	}
+	
+	public void deleteUser(String id) {
+		userRepository.deleteById(id);
+	}
+	
+	public boolean Login(String name, String password) {
+		Optional<UserAuthData> userOpt = userRepository.findById(name);
+		return userOpt.isPresent() && userOpt.get().getPassword().equals(password);
+	}
+	
+	public Optional<UserAuthData> getUserById(String id) {
+        return userRepository.findById(id);
+    }
+}
